6846eed80e4124205490ae67feed25ff511cfe77
Gaogao Han <firstName.lastName@corp.trizetto.com>
2022-09-11 14:42:13 -0400

    Add Entiry into the Model, add HttpClient link to Web API and model into Service

--- PaymentApp1/src/app/shared/payment-detail.service.ts
+++ PaymentApp1/src/app/shared/payment-detail.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  readonly baseURL = 'https://localhost:44343/api/PaymentDetails'
+  formData: PaymentDetail = new PaymentDetail();
 }
