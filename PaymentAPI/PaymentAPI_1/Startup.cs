15c3ba57a2b9fb45f578ca1dccfea453ccfa652c
Gaogao Han <firstName.lastName@corp.trizetto.com>
2022-09-10 22:32:26 -0400

    Update configure in startup.cs

--- PaymentAPI/PaymentAPI_1/Startup.cs
+++ PaymentAPI/PaymentAPI_1/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PaymentAPI.Models;
 
 namespace PaymentAPI
 {
@@ -32,6 +27,9 @@ namespace PaymentAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
             });
+            services.AddDbContext<PaymentDetailContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
