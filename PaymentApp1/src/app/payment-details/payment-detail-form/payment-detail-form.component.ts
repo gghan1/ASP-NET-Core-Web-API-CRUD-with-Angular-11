a93deaf4d60669019bf3e7f6b52ab82012e2b34b
Gaogao Han <firstName.lastName@corp.trizetto.com>
2022-09-23 20:21:26 -0400

    show details form

--- PaymentApp1/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp1/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { PaymentDetailService } from '../../shared/payment-detail.service';
 
 @Component({
