77410fc612ea4cc6384a47d812464b8156134405
Gaogao Han <firstName.lastName@corp.trizetto.com>
2022-09-26 10:30:38 -0400

    save

--- PaymentApp1/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp1/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaymentDetail } from '../../shared/payment-detail.model';
 import { PaymentDetailService } from '../../shared/payment-detail.service';
 
 @Component({
@@ -22,10 +23,15 @@ export class PaymentDetailFormComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.service.pstPaymentDetail().subscribe(
       res => {
-
+        this.resetForm(form);
       },
       err => { console.log(err) }
     )
+  }
+
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
   }
 
 }
